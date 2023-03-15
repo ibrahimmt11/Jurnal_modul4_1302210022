dfdf6b54faf370b0aa3687ebade1be9efaf4235f
ibrahimmt11 <127002385+ibrahimmt11@users.noreply.github.com>
2023-03-15 16:34:51 +0700

    Menambah class KodeBuah #2

---
+++ KodeBuah.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302210022
+{
+    internal class KodeBuah
+    {
+        public enum NamaBuah
+        {
+            Apel,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung,
+            Kurma,
+            Durian,
+            Anggur,
+            Melon,
+            Semangka
+        }
+
+        public class getKode
+        {
+            public static string getKodeBuah(NamaBuah namaBuah)
+            {
+                string[] kodeList = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+                return kodeList[(int)namaBuah];
+            }
+        }
+    }
+}
