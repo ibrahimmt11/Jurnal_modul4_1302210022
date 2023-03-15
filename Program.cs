b7085f64bb67c7269ad24770e7756aa8abc19f70
ibrahimmt11 <127002385+ibrahimmt11@users.noreply.github.com>
2023-03-15 15:57:26 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using static KodeBuah;
+
+public class KodeBuah
+{
+    public enum NamaBuah
+    {
+        Apel,
+        Aprikot,
+        Alpukat,
+        Pisang,
+        Paprika,
+        Blackberry,
+        Ceri,
+        Kelapa,
+        Jagung
+    }
+
+    public class
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
