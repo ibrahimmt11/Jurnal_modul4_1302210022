eca41c2bfe414e1699baf2bf04cb5c65c1182cc6
ibrahimmt11 <127002385+ibrahimmt11@users.noreply.github.com>
2023-03-15 17:13:34 +0700

    menambahkan class PosisiKarakterGame

---
+++ PosisiKarakterGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302210022
+{
+    internal class PosisiKarakterGame
+    {
+        public enum State
+        {
+            Berdiri,
+            Jongkok,
+            Tengkurap,
+            Terbang
+        }
+
+        public enum Button
+        {
+            W,
+            S,
+            x
+        }
+
+        State kondisi;
+
+        public PosisiKarakterGame()
+        {
+            kondisi = State.Berdiri;
+            Console.WriteLine("Posisi Berdiri");
+        }
+
+        public void PBerdiri()
+        {
+            if(kondisi== State.Berdiri)
+            {
+                kondisi = State.Berdiri;
+                Console.WriteLine("Posisi standby");
+            }
+        }
+
+        public void PTengkurap()
+        {
+            if(kondisi== State.Berdiri)
+            {
+                kondisi= State.Tengkurap;
+                Console.WriteLine("Posisi istirahat");
+            }
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
-﻿using static modul4_1302210022.KodeBuah;
+﻿using modul4_1302210022;
+using static modul4_1302210022.KodeBuah;
+using static modul4_1302210022.PosisiKarakterGame;
 
 class Program
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("=== Table Driven ===");
         getKode kodeBuah= new getKode();
         NamaBuah buah = NamaBuah.Apel;
         string kode = getKode.getKodeBuah(buah);
         Console.WriteLine(buah + ": " + kode);
 
+        Console.WriteLine("=== State-base ===");
+        PosisiKarakterGame orang = new PosisiKarakterGame();
+        orang.PBerdiri();
+        orang.PTengkurap();
     }
 }
